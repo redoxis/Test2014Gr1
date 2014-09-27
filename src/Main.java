d28099c1555ff3cd6f4aaef8fbd83cf68eccefb3
redoxis <radek.romanowski93@gmail.com>
2014-09-27 12:15:35 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
 		System.out.println("Hello World !");
 		System.out.print("Hello World 2 !");
+		System.out.print("Hello World 3 !");
+		System.out.print("Hello World 4 !");
 	}
 
 }
